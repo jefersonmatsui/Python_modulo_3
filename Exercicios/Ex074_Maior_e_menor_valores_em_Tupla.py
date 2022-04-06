9adcb8d947f240a0b731e56a0708d60562403551
jefersonmatsui <jefersonmatsui@gmail.com>
2022-04-05 23:47:23 -0300

    Novo

---
+++ Exercicios/Ex074_Maior_e_menor_valores_em_Tupla.py
@@ -0,0 +1,12 @@
+# Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
+# Depois disso, mostre a listagem de números gerados e
+# também indique o menor e o maior valor que estão na tupla.
+
+from random import randint
+num = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print('Os valores sorteados foram: ', end='')
+for n in num:
+    print(f'{n} ', end='')
+print(f'\nO maior valor sorteado foi {max(num)}')
+print(f'O menor valor sorteado foi {min(num)}')
+
