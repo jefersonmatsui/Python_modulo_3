6d6651285f84afcabeca3b947171cdc5c69a7608
jefersonmatsui <jefersonmatsui@gmail.com>
2022-05-10 18:06:49 -0300

    Novo

--- Exercicios/Ex085_Listas_com_pares_e_impares.py
+++ Exercicios/Ex085_Listas_com_pares_e_impares.py
@@ -7,7 +7,7 @@ lista = [[], []]
 valor = 0
 for c in range(1, 8):
     valor = int(input(f'Digite o {c}° valor: '))
-    if valor % 2 ==0:
+    if valor % 2 == 0:
         lista[0].append(valor)
     else:
         lista[1].append(valor)
